5e7080caec8c244f06fbe0317c62ed276b7186ab
sharkk2 <129411155+sharkk2@users.noreply.github.com>
2025-02-06 13:20:25 +0200

    Delete src/main/java/org/discraft/bot directory

--- src/main/java/org/discraft/bot/core/classes/SlashCommand.java
+++
@@ -1,8 +0,0 @@
-package org.discraft.bot.core.classes;
-
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
-
-public interface SlashCommand {
-    SlashCommandData getCommandData();
-    void execute(net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent event);
-}
