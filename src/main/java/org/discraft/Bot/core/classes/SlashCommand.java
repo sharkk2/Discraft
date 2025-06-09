5005f8494344728a27337773416d884b0ef27295
sharkk2 <129411155+sharkk2@users.noreply.github.com>
2025-06-09 21:14:02 +0300

    Update SlashCommand.java

--- src/main/java/org/discraft/Bot/core/classes/SlashCommand.java
+++ src/main/java/org/discraft/Bot/core/classes/SlashCommand.java
@@ -1,8 +1,9 @@
 package org.discraft.Bot.core.classes;
 
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import org.discraft.Discraft;
 
 public interface SlashCommand {
     SlashCommandData getCommandData();
-    void execute(net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent event);
+    void execute(net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent event, Discraft plugin);
 }
